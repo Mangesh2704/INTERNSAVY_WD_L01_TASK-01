3ff1846cbf4584da3cecab5ec12dd9bd3609fcbe
Mangesh Pangam <135334519+Mangesh2704@users.noreply.github.com>
2024-01-09 23:14:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware for parsing JSON and urlencoded form data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files (in this case, the HTML and background image)
 app.use(express.static('public'));
 
-// Initialize NeDB database
 const db = new Datastore({ filename: 'formdata.db', autoload: true });
 
-// Endpoint to handle form submission
 app.post('/submit', (req, res) => {
   const formData = req.body;
 
-  // Insert form data into NeDB
   db.insert(formData, (err, newDoc) => {
     if (err) {
       console.error(err);
@@ -32,12 +27,10 @@ app.post('/submit', (req, res) => {
   });
 });
 
-// Serve the HTML file for the root path
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
